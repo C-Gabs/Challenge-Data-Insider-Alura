6b104fdada29d691f485cd16ad7f4a890cb94b1a
GabsDev <gabrielcastropds@gmail.com>
2025-08-06 14:33:58 -0400

    No hay cambios relevantes

--- app/st_data_sicence.py
+++ app/st_data_sicence.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 import plotly.graph_objects as go
+import matplotlib.pyplot as plt
 from datetime import datetime
 from visualizaciones import bar_char, hbar_char, scatter_char
 
